0c8d18c31e206522137a4618fea11c218400efa3
anujsharma-bbd <sharmaanuj@hotmail.com>
2019-09-14 02:25:10 +0530

    added multiple photos functionalities

--- index.js
+++ index.js
@@ -7,16 +7,14 @@ class App extends Component {
   imageRefs = [];
   constructor() {
     super();
-    this.state = { start: false, isError: false, isLoading: false };
+    this.state = { start: false, isError: false, isLoading: false, takePhotoCount: 0, images: [] };
     this.start = this.start.bind(this);
     this.stop = this.stop.bind(this);
     this.pause = this.pause.bind(this);
     this.TakePhoto = this.TakePhoto.bind(this);
-    this.setRef = this.setRef.bind(this);
     this.remove = this.remove.bind(this);
     this.player = React.createRef();
     this.canvas = React.createRef();
-    this.image = React.createRef();
   }
   start() {
     console.log(this.refs)
@@ -35,26 +33,28 @@ class App extends Component {
         })
     }
   }
-  setRef(ref) {
-    this.imageRefs.push(ref);
-  };
   remove(ref) {
 
   }
   TakePhoto() {
-
-    // first way
     const context = this.canvas.getContext('2d');
-    context.drawImage(this.player, 0, 0, this.canvas.width, this.canvas.height); // generic way but if we want to see base64 , we must show in <img tag
-    // first way END
-
-    // second way Start
-    this.image.src = this.canvas.toDataURL('image/png');
-    // second way END
-    this.stop();
+    context.drawImage(this.player, 0, 0, this.canvas.width, this.canvas.height);
+    this.setState({ takePhotoCount: (this.state.takePhotoCount + 1) }, () => {
+      let count = this.state.takePhotoCount;
+      this.setState({
+        images: [...this.state.images, {
+          count,
+          url: this.canvas.toDataURL('image/png')
+        }]
+      });
+    });
+    // this.stop();
   }
   pause() {
-    this.player.pause();
+    if (!this.player.paused)
+      this.player.pause();
+    else
+      this.player.play();
   }
   stop() {
     this.player.srcObject.getVideoTracks()
@@ -69,9 +69,9 @@ class App extends Component {
     });
   }
   render() {
-    const { isError, errorMessage, start } = this.state;
+    const { isError, errorMessage, start, images } = this.state;
     return (
-      <div>
+      <div className='container-fluid'>
         <br />
         <p >
           <button onClick={this.start} className='btn btn-success m-2' disabled={start}>Start Video</button>
@@ -79,24 +79,22 @@ class App extends Component {
           <button onClick={this.pause} disabled={!start} className='btn btn-danger m-2'>Pause Video</button>
           <button onClick={this.TakePhoto} disabled={!start} className='btn btn-danger m-2'>Take Photo</button>
         </p>
-        {
-
-        }
-        <p >
-
-        </p>
-        <p className='player-container'>
-          <video id='player' autoPlay={true} ref={x => this.player = x}
-            poster="https://cdn2.iconfinder.com/data/icons/pixel-perfect-at-24px-volume-8-1/24/mic-video-512.png"
-          ></video>
-        </p>
-        <p>
-          {
-            this.imageRefs.map(image =>
-              <img ref={this.setRef} key={image} width='320' height='240' alt='' />
-            )
-          }
-        </p>
+        <div className='row'>
+          <div className='col-sm-6'>
+            <p className='player-container'>
+              <video id='player' autoPlay={true} ref={x => this.player = x}
+                poster="https://cdn2.iconfinder.com/data/icons/pixel-perfect-at-24px-volume-8-1/24/mic-video-512.png"
+              ></video>
+            </p>
+          </div>
+          <div className='col-sm-6 all-images'>
+            {
+              images.map((img, index) =>
+                <img key={'image-' + index} src={img.url} width='320' className='m-2' style={{ float: 'left' }} height='240' alt='' />
+              )
+            }
+          </div>
+        </div>
         <p>
           <canvas style={{ display: 'none' }} ref={x => this.canvas = x} id='canvas' width='320' height='240'></canvas>
         </p>
