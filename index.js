6a045ddb83e0d0c5b3fbac565dc4ef2364cd27c3
anujsharma-bbd <sharmaanuj@hotmail.com>
2019-09-13 16:16:28 +0530

    added webcam functionality

--- index.js
+++ index.js
@@ -10,9 +10,13 @@ class App extends Component {
     this.start = this.start.bind(this);
     this.stop = this.stop.bind(this);
     this.pause = this.pause.bind(this);
-    this.player = React.createRef;
+    this.TakePhoto = this.TakePhoto.bind(this);
+    this.player = React.createRef();
+    this.canvas = React.createRef();
+    this.image = React.createRef();
   }
   start() {
+    console.log(this.refs)
     if (navigator.mediaDevices.getUserMedia) {
       navigator.mediaDevices.getUserMedia({ video: true, audio: true })
         .then(stream => {
@@ -28,15 +32,26 @@ class App extends Component {
         })
     }
   }
+  TakePhoto() {
+
+    // first way
+    const context = this.canvas.getContext('2d');
+    context.drawImage(this.player, 0, 0, this.canvas.width, this.canvas.height); // generic way but if we want to see base64 , we must show in <img tag
+    // first way END
+
+    // second way Start
+    this.image.src = this.canvas.toDataURL('image/png');
+    // second way END
+    this.stop();
+  }
   pause() {
     this.player.pause();
   }
   stop() {
-    let stream = this.player.srcObject;
-    var tracks = stream.getTracks();
-    tracks.forEach(x => {
-      x.stop();
-    })
+    this.player.srcObject.getVideoTracks()
+      .forEach(x => {
+        x.stop();
+      })
     this.player.srcObject = null;
     this.setState({
       isError: false,
@@ -52,7 +67,8 @@ class App extends Component {
         <p >
           <button onClick={this.start} className='btn btn-success m-2' disabled={start}>Start Video</button>
           <button onClick={this.stop} disabled={!start} className='btn btn-danger m-2'>Stop Video</button>
-          <button onClick={this.secondary} disabled={!start} className='btn btn-danger m-2'>Pause Video</button>
+          <button onClick={this.pause} disabled={!start} className='btn btn-danger m-2'>Pause Video</button>
+          <button onClick={this.TakePhoto} disabled={!start} className='btn btn-danger m-2'>Take Photo</button>
         </p>
         {
 
@@ -64,6 +80,10 @@ class App extends Component {
           <video id='player' autoPlay={true} ref={x => this.player = x}
             poster="https://cdn2.iconfinder.com/data/icons/pixel-perfect-at-24px-volume-8-1/24/mic-video-512.png"
           ></video>
+        </p>
+        <p>
+          <canvas style={{ display: 'none' }} ref={x => this.canvas = x} id='canvas' width='320' height='240'></canvas>
+          <img ref={x => this.image = x} id='image' width='320' height='240' alt='' />
         </p>
       </div >
     );
